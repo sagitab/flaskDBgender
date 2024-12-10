de071baf47818410ba3bdd28f447e1aac7e81e08
sagitab <sagilavy01@gmail.com>
2024-12-10 14:19:51 +0200

    add add image page

--- db-scripts/init.sql
+++ db-scripts/init.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE IF NOT EXISTS mydb;
+
+USE mydb;
+
 CREATE TABLE IF NOT EXISTS `imges` (
   `idimges` INT NOT NULL AUTO_INCREMENT,
   `url` VARCHAR(500) NULL,
@@ -5,6 +9,19 @@ CREATE TABLE IF NOT EXISTS `imges` (
   PRIMARY KEY (`idimges`)
 );
 
+CREATE TABLE IF NOT EXISTS `count` (
+  `idcounter` INT NOT NULL AUTO_INCREMENT,
+  `counter` INT NOT NULL,
+  PRIMARY KEY (`idcounter`)
+);
+
+-- Insert an initial value for the counter
+INSERT INTO `count` (`counter`) VALUES (0);
+
+-- Insert image URLs and names
 INSERT INTO `imges` (url, name) 
 VALUES 
-  ("https://www.chipublib.org/wp-content/uploads/sites/3/2022/09/36079964425_7b3042d5e1_k.jpg", "eminem");
+  ("https://www.chipublib.org/wp-content/uploads/sites/3/2022/09/36079964425_7b3042d5e1_k.jpg", "eminem"),
+  ("https://i.scdn.co/image/ab6761610000e5eba00b11c129b27a88fc72f36b", "eminem"),
+  ("https://ih1.redbubble.net/image.3270018746.5019/flat,750x,075,f-pad,750x1000,f8f8f8.jpg", "eminem"),
+  ("https://www.aestheticwalldecor.com/cdn/shop/files/eminem-rap-rolling-stone-magazine-wall-art-poster-aesthetic-wall-decor.jpg?v=1692556007", "eminem");
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, jsonify
 import requests
 import mysql.connector
 import os
-
+import random
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -11,10 +12,22 @@ app = Flask(__name__)
 # app.config['MYSQL_USER'] = 'root'
 # app.config['MYSQL_PASSWORD'] = 'DataL123'
 # app.config['MYSQL_DB'] = 'mydb'
+app.config['PORT'] = os.getenv("PORT")
 app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST") # Make sure this matches the service name in Docker Compose
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+app.config['COUNTER']=0
+
+# Set the upload folder and allowed extensions
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = os.path.join('static', UPLOAD_FOLDER)
+
+# Function to check allowed file extensions
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 # Connect to MySQL
 def get_db_connection():
@@ -34,21 +47,111 @@ def getPic():
     data = cursor.fetchall()
     cursor.close()
     conn.close()
+    pic_urls = [item["url"] for item in data]
+    print(pic_urls)
+   
+
 
     if data:
         # Return the first image URL from the data
-        pic = data[0]["url"]
+        #pic = data[0]["url"]
+            # Get a random item
+        #random_item = random.choice(pic_urls)
+        random_item=pic_urls[-1]
+        pic=random_item
+        if not pic.startswith("https://"):
+            pic = os.path.join("uploads", pic)
+            pic = os.path.join("static", pic)
+            
+
     else:
         pic = None  # Handle case where no data is found
     return pic
+def getCounter():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM count;")  
+    data = cursor.fetchone()
+    print("#########################################################")
+    print(data)
+    cursor.close()
+    conn.close()
+    counter = int(data["counter"])
+
+    return counter
+def updateCounter(counter):
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        # Use parameterized queries to avoid SQL injection risks
+        query = "UPDATE count SET counter = %s WHERE idcounter = 1;"
+        ret = cursor.execute(query, (counter,))
+        
+        # Commit the transaction to apply the update
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+        return ret
+    except Exception as e:
+        print(f"Error: {e}")  # Log the error message
+        return False
+def insert_img(name,path):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+   # Use %s for placeholders in MySQL
+    cursor.execute("INSERT INTO imges (name, url) VALUES (%s, %s)", (name, path))
+    conn.commit()  # Commit the transaction
+    cursor.close()
+    conn.close()
 
 # Genderize.io API URL
 API_URL = "https://api.genderize.io"
 
 @app.route('/')
 def index():
+    counter=getCounter()
+    counter+=1
+    if updateCounter(counter):
+        print("ok")
+    else:
+        print("nooooo")    
     pic = getPic()
-    return render_template('index.html', src=pic)
+    return render_template('index.html', src=pic,visits=counter)
+@app.route('/add_img')
+def add_img():
+    return render_template('add_img.html')
+@app.route('/postAddImg', methods=['POST'])
+def post_add_img():
+     # Check if the request has the file part
+    if 'image' not in request.files:
+        msg="error!!"
+        render_template('add_img.html',msg=msg)
+    file = request.files['image']
+    
+    # If no file is selected
+    if file.filename == '':
+        msg="error!!"
+        render_template('add_img.html',msg=msg)
+    
+    # Check if the file is allowed
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+
+        # Get the name from the form
+        name = request.form['name']
+        #formatted_src = filepath.replace('/', '\\')
+        insert_img(name,filename)
+        msg="success!!"
+    else:
+         msg="error!!"
+
+
+    return render_template('add_img.html',msg=msg)
+
+   
 
 @app.route('/detect_gender', methods=['POST'])
 def detect_gender():
@@ -61,7 +164,8 @@ def detect_gender():
         gender = "male with pussy"
         is_gay = "Yes"
         probability = "100"
-        return render_template('index.html', name=name, is_gay=is_gay, probability=probability, gender=gender, src=pic)
+        counter=int(app.config['COUNTER'])
+        return render_template('index.html', name=name, is_gay=is_gay, probability=probability, gender=gender, src=pic,visits=counter)
 
     try:
         # Call Genderize.io API
@@ -84,4 +188,5 @@ def detect_gender():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port=app.config['PORT']
+    app.run(host='0.0.0.0',port=int(os.getenv('PORT',5000)) )
