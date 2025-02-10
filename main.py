ddce506ec013a8b04f332b4a7efc6c7c99c99213
sagitab <sagilavy01@gmail.com>
2025-02-10 13:42:46 +0200

    update monitor service

--- main.py
+++ main.py
@@ -161,7 +161,9 @@ def post_add_img():
 
 @app.route('/metrics')
 def metrics():
-    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+    # Return all metrics in Prometheus format
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
 @app.route('/detect_gender', methods=['POST'])
 def detect_gender():
     pic = getPic()
