567ab699550a5253453c70cdce76e3ddfe1e2985
sagitab <sagilavy01@gmail.com>
2025-03-18 22:06:31 +0200

    try docker clean

--- docker_clean.sh
+++ docker_clean.sh
@@ -3,17 +3,18 @@ DOCKER_USERNAME="$1"
 DOCKER_PASSWORD="$2"
 REPO_NAME="flaskaws"
 
-# Get list of tags, sort numerically
+# Fetch all image tags from Docker Hub and sort them numerically
 TAGS=$(curl -s -H "Authorization: Bearer $DOCKER_PASSWORD" \
 "https://hub.docker.com/v2/repositories/$DOCKER_USERNAME/$REPO_NAME/tags/?page_size=1000" | jq -r ".results[].name" | sort -V)
 
-# Convert to array
-TAG_ARRAY=($TAGS)
+# Store tags in an array (correct way to handle multiline output)
+mapfile -t TAG_ARRAY <<< "$TAGS"
+
 KEEP_COUNT=20
 TOTAL_TAGS=${#TAG_ARRAY[@]}
 
 if (( TOTAL_TAGS > KEEP_COUNT )); then
-  DELETE_TAGS=("${TAG_ARRAY[@]:0:TOTAL_TAGS-KEEP_COUNT}")  
+  DELETE_TAGS=("${TAG_ARRAY[@]:0:TOTAL_TAGS-KEEP_COUNT}")
 
   for TAG in "${DELETE_TAGS[@]}"; do
     if [[ "$TAG" != "141#" ]]; then
@@ -26,4 +27,4 @@ if (( TOTAL_TAGS > KEEP_COUNT )); then
   done
 else
   echo "No old images to delete. Less than or equal to $KEEP_COUNT images exist."
-
+fi  # ✅ Missing 'fi' is now added
