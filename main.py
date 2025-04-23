3d7e11275806a3d6f0e5a957e40207e522488f1d
sagitab <sagilavy01@gmail.com>
2025-04-23 19:38:58 +0300

    css and problom

--- main.py
+++ main.py
@@ -16,17 +16,26 @@ VISITOR_COUNTER = Gauge('flask_app_visitor_count', 'Current number of visitors')
 
 app = Flask(__name__)
 
-# MySQL Database Configuration
-# app.config['MYSQL_HOST'] = 'mysql' # Make sure this matches the service name in Docker Compose
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'DataL123'
-# app.config['MYSQL_DB'] = 'mydb'
 app.config['PORT'] = os.getenv("PORT")
 app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST") # Make sure this matches the service name in Docker Compose
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 app.config['COUNTER']=0
+# # Debugging output
+# config_vars = {
+#     "PORT": app.config['PORT'],
+#     "MYSQL_HOST": app.config['MYSQL_HOST'],
+#     "MYSQL_USER": app.config['MYSQL_USER'],
+#     "MYSQL_PASSWORD": app.config['MYSQL_PASSWORD'],
+#     "MYSQL_DB": app.config['MYSQL_DB']
+# }
+
+# for var, value in config_vars.items():
+#     if value:
+#         print(f"{var}: {value}")
+#     else:
+#         print(f"ERROR: {var} is not set!")
 
 # Set the upload folder and allowed extensions
 UPLOAD_FOLDER = 'uploads'
@@ -114,8 +123,7 @@ def insert_img(name,path):
     cursor.close()
     conn.close()
 
-# Genderize.io API URL
-API_URL = "https://api.genderize.io"
+
 @REQUEST_TIME.time()
 @app.route('/')
 def index():
@@ -130,9 +138,9 @@ def index():
     logging.debug(f"Returning with {counter} visits and picture {pic}")
     return render_template('index.html', src=pic, visits=counter)
 
-@app.route('/add_img')
-def add_img():
-    return render_template('add_img.html')
+# @app.route('/add_img')
+# def add_img():
+#     return render_template('add_img.html')
 @app.route('/postAddImg', methods=['POST'])
 def post_add_img():
      # Check if the request has the file part
@@ -170,40 +178,6 @@ def metrics():
     VISITOR_COUNTER._value.set(counter) 
     # Return all metrics in Prometheus format
     return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
-
-@app.route('/detect_gender', methods=['POST'])
-def detect_gender():
-    pic = getPic()
-    # Get the name from the form
-    name = request.form.get('name')
-    if not name:
-        return jsonify({"error": "Name is required"}), 400
-    if name == "amity":
-        gender = "male with pussy"
-        is_gay = "Yes"
-        probability = "100"
-        counter=int(app.config['COUNTER'])
-        return render_template('index.html', name=name, is_gay=is_gay, probability=probability, gender=gender, src=pic,visits=counter)
-
-    try:
-        # Call Genderize.io API
-        response = requests.get(API_URL, params={"name": name})
-        data = response.json()
-
-        # Get gender and probability from API response
-        gender = data.get('gender')
-        probability = data.get('probability', 0) * 100  # Convert probability to percentage
-
-        # Assuming you're using gender to determine the "is_gay" logic (you may want to reconsider this)
-        is_gay = "No"
-        if gender == "female":
-            is_gay = "Yes"  # Placeholder logic for demonstration
-
-        # Render the result on the HTML page
-        return render_template('index.html', name=name, is_gay=is_gay, probability=probability, gender=gender, src=pic)
-
-    except requests.RequestException as e:
-        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     port=app.config['PORT']
